f03e236ec2bb9d21dd2ce632faa165f380521b45
Alde Roberge <rotmgiobsidian@gmail.com>
2019-09-08 17:02:06 -0400

    Added foreground and pillars

---
+++ Assets/EmitABunchOfFallingObjects.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmitABunchOfFallingObjects : MonoBehaviour
+{
+    public GameObject fallingObjectPrefab;
+    
+    
+    private float minRespawn = 1;
+    private float maxRespawn = 2;
+
+    void Start()
+    {
+        Invoke (nameof(YourMethod), (Random.Range(minRespawn, maxRespawn)));
+    }
+ 
+    void YourMethod()
+    {
+        float delay = Random.Range (minRespawn, maxRespawn);
+        //Do stuff
+
+        Object.Instantiate(fallingObjectPrefab);
+        fallingObjectPrefab.transform.parent = this.gameObject.transform;
+        
+        Invoke (nameof(YourMethod), delay);
+    }
+    
+    
+    
+    
+    
+}
